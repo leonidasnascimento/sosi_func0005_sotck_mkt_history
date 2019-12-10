1120b135b4234f4ef720d4a3a42252ada449c87e
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2019-12-10 13:24:32 -0300

    Adjustments to improve perfomance

--- func/__init__.py
+++ func/__init__.py
@@ -25,12 +25,11 @@ def main(func: func.TimerRequest) -> None:
 
         config_obj: reader = reader(SETTINGS_FILE_PATH, 'Values')
         post_service_url: str = config_obj.get_value("post_service_url")
-        defined_start_date: str = config_obj.get_value("start_date") # Expected format DD/MM/YYYY
-        defined_end_date: str = config_obj.get_value("end_date") # Expected format DD/MM/YYYY
         formatted_start_date: int = 0
         formatted_end_date: int = 0
         target_url: str = config_obj.get_value("target_url")
         stock_code_list_service_url: str = config_obj.get_value("stock_code_list_service_url")
+        days_to_look_back: str = config_obj.get_value("days_to_look_back", "0")
         response: requests.Response = None
         stk_codes: array.array = {}
         date_parse_pattern: str = "%d/%m/%Y"
@@ -38,40 +37,28 @@ def main(func: func.TimerRequest) -> None:
         # Crawling
         logging.info("Getting stock market history list. It may take a while...")
 
-        if(defined_start_date != "" and defined_end_date != ""):
-            logging.info("From {} to {}".format(defined_start_date, defined_end_date))
+        start_date: datetime.date = datetime.date.today() - relativedelta(days=int(days_to_look_back))
+        formatted_start_date = int(time.mktime(start_date.timetuple()))
+        formatted_end_date = int(time.mktime(datetime.date.today().timetuple()))
 
-            start_date_aux: datetime.date = datetime.datetime.strptime(defined_start_date, date_parse_pattern)
-            end_date_aux: datetime.date = datetime.datetime.strptime(defined_end_date, date_parse_pattern)
-
-            formatted_start_date = int(time.mktime(start_date_aux.timetuple()))
-            formatted_end_date = int(time.mktime(end_date_aux.timetuple()))
-        else:
-            logging.warning("'start_date' and 'end_date' are required. Using default date for period: Last week")
-
-            last_week_aux: datetime.date = datetime.date.today() - relativedelta(days=7)
-            formatted_start_date = int(time.mktime(last_week_aux.timetuple()))
-            formatted_end_date = int(time.mktime(datetime.date.today().timetuple()))
-            pass
+        logging.info("From {} to {}".format(start_date.strftime(date_parse_pattern), datetime.date.today().strftime(date_parse_pattern)))
 
         # Getting stock code list
         response = requests.request("GET", stock_code_list_service_url)
         stk_codes = json.loads(response.text)
-        crawler_obj: Crawler = Crawler(target_url, formatted_start_date, formatted_end_date)
-
+        thread_lst: List[threading.Thread] = []
+        
         for code in stk_codes:
-            stock_hist: Stock = crawler_obj.get_history(code['stock'])
+            t_aux: threading.Thread = threading.Thread(target=process_crawling, args=(code['stock'], target_url, post_service_url, formatted_start_date, formatted_end_date))            
+            thread_lst.append(t_aux)
             
-            if stock_hist:
-                if not stock_hist.history:
-                    stock_hist.history = []    
-
-                json_obj = json.dumps(stock_hist.__dict__, default=lambda o: o.__dict__)
-
-                threading.Thread(target=invoke_url, args=(post_service_url, json_obj)).start()
-                logging.info("'{}' sent for data base persistence...".format(code['stock']))                
-                pass
+            t_aux.start()
             pass
+            
+        # Wait 'till all threads are done
+        for thread in thread_lst:
+            if thread and thread.is_alive():
+                thread.join()
 
         logging.info("Timer job is done. Waiting for the next execution time")
 
@@ -89,4 +76,20 @@ def invoke_url(url, json):
     }
 
     requests.request("POST", url, data=json, headers=headers)
+    pass
+
+def process_crawling(stock_code: str, target_url: str, post_service_url: str, formatted_start_date: int, formatted_end_date: int):
+    crawler_obj: Crawler = Crawler(target_url, formatted_start_date, formatted_end_date)
+    stock_hist: Stock = crawler_obj.get_history(stock_code)
+
+    if stock_hist:
+        if not stock_hist.history:
+            stock_hist.history = []    
+
+        json_obj = json.dumps(stock_hist.__dict__, default=lambda o: o.__dict__)
+
+        threading.Thread(target=invoke_url, args=(post_service_url, json_obj)).start()
+
+        logging.info("'{}' sent for data base persistence...".format(stock_code))                
+        pass
     pass
